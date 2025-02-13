290b5babd8820edbb67110b4bf72f5fda79f026f
adzashel <seigneur.noir15@gmai.com>
2025-02-13 14:44:37 +0700

    bug time

--- src/views/Forecast.jsx
+++ src/views/Forecast.jsx
@@ -1,22 +1,49 @@
 /* eslint-disable no-undef */
 import weatherIcons from '../constant'
 const Forecast = ({ hourly }) => {
-  const time = hourly.time;
-  const temp = Math.floor(hourly.mintemp_c)
-  const weatherIcon = Object.keys(weatherIcons).find((icon) => weatherIcons[icon].includes(hourly.condition.code))
+ 
+  const temp = Math.floor(hourly.temp_c)
+  const feelsLike = Math.floor(hourly.feelslike_c);
+  const weatherIcon = Object.keys(weatherIcons).find((icon) => weatherIcons[icon].includes(hourly.condition.code));
+
+
+  // convert time epoch to 12 hours format
+  const convertTimeEpoch = (timeEpoch , format24 = true ) => {
+
+    // create date object
+    const date = new Date(timeEpoch * 1000);
+
+    // get hours , minutes , and pm am
+    let hours = date.toLocaleString();
+    const minutes = date.getMinutes().toString().padStart(2, '0'); // leading the 0 if needed
+    let amPm = "AM";
+
+    // convert to 12 hours format if necessary
+    if(!format24) {
+      if(hours >= 12) {
+        amPm = "PM";
+      }
+      hours = hours % 12 || 12; // Now adjust hours for 12-hour format
+    }
+    // const time= `${hours}:${minutes} : ${amPm}`
+    // console.log(time);
+    return `${hours}:${minutes} : ${amPm}`;
+  }
+
+  
   return (
     <div  className="forecast-item">
-                  <p className="weather-temp">{ time }</p>
+                  <p className="weather-temp">{ convertTimeEpoch(hourly.time_epoch , false) }</p>
                   <img
                     src={`src/icons/${ weatherIcon }.svg`}
                     alt=""
                     className="weather-icon"
                   />
                   <span className="day">
-                    { temp }
+                    { temp }°C
                   </span>
                   <span className="days">
-                    / 37C
+                    / { feelsLike}°C
                   </span>
                 </div>
   );
